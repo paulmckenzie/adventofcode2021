4e9ca2ffde28eba0fae3c358bb67dc2a16255264
pmk <paul.mckenzie@gmail.com>
2023-03-08 10:32:23 +0000

    Day One, using windowed list and map (also fixed test dependencies)

--- src/test/kotlin/DayOne.kt
+++ src/test/kotlin/DayOne.kt
@@ -1,8 +1,31 @@
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.equalTo
 import org.junit.jupiter.api.Test
+
+/**
+ * https://adventofcode.com/2021/day/1
+ */
 
 class DayOne {
     @Test
-    fun `ah ha`() {
-        TODO("Not yet implemented")
+    fun `depth scanning`() {
+        val input = listOf(199, 200, 208, 210, 200, 207, 240, 269, 260, 263)
+        val expected = listOf(
+            "increased",
+            "increased",
+            "increased",
+            "decreased",
+            "increased",
+            "increased",
+            "increased",
+            "decreased",
+            "increased"
+        )
+
+        val actual = input
+            .windowed(2, 1)
+            .map { depthPair -> if (depthPair[0] > depthPair[1]) "decreased" else "increased" }
+
+        assertThat(actual, equalTo(expected))
     }
 }
